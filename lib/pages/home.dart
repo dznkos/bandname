9edfe8062819f25a670e3e07732bb848e97868ac
Luis Vice <khanccer@gmail.com>
2020-10-23 16:50:47 -0500

    version 0.6

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,7 +12,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Band> bands = [
     Band(id: '1', name: 'The Killers', votes: 5),
-    Band(id: '2', name: '', votes: 2),
+    Band(id: '2', name: 'Son son', votes: 2),
     Band(id: '3', name: 'Van Van', votes: 6),
     Band(id: '4', name: 'Gran Combo', votes: 3),
   ];
@@ -46,13 +46,29 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  ListTile _bandTile(Band band) {
-    return ListTile(
-      leading: CircleAvatar(
-        child: Text(band.name.substring(0, 2)),
+  Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        // eliminar en DB
+      },
+      background: Container(
+        padding: EdgeInsets.only(left: 8.0),
+        color: Colors.red,
+        child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Delete Band', style: TextStyle(color: Colors.white))),
       ),
-      title: Text('${band.name}'),
-      trailing: Text('${band.id}'),
+      child: Container(
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Text(band.name.substring(0, 2)),
+          ),
+          title: Text('${band.name}'),
+          trailing: Text('${band.id}'),
+        ),
+      ),
     );
   }
 
@@ -81,8 +97,8 @@ class _HomePageState extends State<HomePage> {
     }
 
     showCupertinoDialog(
-      context: context, 
-      builder: ( _ ) {
+      context: context,
+      builder: (_) {
         return CupertinoAlertDialog(
           title: Text('New Band name:'),
           content: CupertinoTextField(
@@ -92,23 +108,23 @@ class _HomePageState extends State<HomePage> {
             CupertinoDialogAction(
               isDefaultAction: true,
               child: Text('Add'),
-              onPressed: () => addBandToList( textController.text ),
+              onPressed: () => addBandToList(textController.text),
             ),
             CupertinoDialogAction(
-              isDestructiveAction: true,
-              child: Text('Dismiss'),
-              onPressed: () => Navigator.pop(context)
-            )
+                isDestructiveAction: true,
+                child: Text('Dismiss'),
+                onPressed: () => Navigator.pop(context))
           ],
-        )
+        );
       },
     );
   }
 
   void addBandToList(String name) {
-
     if (name.length > 1) {
-      this.bands.add( new Band(id: DateTime.now().toString(), name: name, votes: 0));
+      this
+          .bands
+          .add(new Band(id: DateTime.now().toString(), name: name, votes: 0));
       setState(() {});
     }
 
