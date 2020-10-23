08fd36a47ce2dec78290acb0e34e2772fd84c589
Luis Vice <khanccer@gmail.com>
2020-10-23 13:54:19 -0500

    version 0.4

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,29 +7,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
   List<Band> bands = [
-    Band(id: '1', name: 'Metalica', votes: 5),
-    Band(id: '2', name: 'Guns and Roses', votes: 2),
+    Band(id: '1', name: 'The Killers', votes: 5),
+    Band(id: '2', name: '', votes: 2),
     Band(id: '3', name: 'Van Van', votes: 6),
     Band(id: '4', name: 'Gran Combo', votes: 3),
-  ]
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BandNames'),
+        title: Center(
+          child: Text(
+            'BandNames',
+            style: TextStyle(color: Colors.blueAccent),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
       ),
       body: ListView.builder(
+        itemCount: bands.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              child: Text( bands[index].name.substring(0,2)),
-            ),
-          )
+          return _bandTile(bands[index]);
         },
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          addNewBand();
+        },
+        elevation: 1,
+      ),
     );
+  }
+
+  ListTile _bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(band.name.substring(0, 2)),
+      ),
+      title: Text('${band.name}'),
+      trailing: Text('${band.id}'),
+    );
+  }
+
+  addNewBand() {
+    final textController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('New band name:'),
+          content: TextField(
+            controller: textController,
+          ),
+          actions: [
+            MaterialButton(
+                child: Text('add'),
+                elevation: 5,
+                textColor: Colors.blue,
+                onPressed: () => addBandToList(textController.text))
+          ],
+        );
+      },
+    );
+  }
+
+  void addBandToList(String name) {
+    if (name.length > 1) {}
+    Navigator.pop(context);
   }
 }
